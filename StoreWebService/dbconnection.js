a190d03ca38d1cedef56c77710637df2b4c824d6
ayatalat <ayatalat.dream@gmail.com>
2017-06-14 16:18:48 +0200

    image

--- StoreWebService/dbconnection.js
+++ StoreWebService/dbconnection.js
@@ -1,10 +1,10 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
 
-host:'localhost',
-user:'root',
-password:'iti',
-database:'Store'
+host:'sql11.freemysqlhosting.net',
+user:'sql11179838',
+password:'XQIX8kHaGS',
+database:'sql11179838'
 
 
 });
