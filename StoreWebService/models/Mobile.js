90c5a286cea31c9ab8ecbc88083a7fae76e496af
ayatalat <ayatalat.dream@gmail.com>
2017-06-17 12:15:12 +0200

    edit mobile

--- StoreWebService/models/Mobile.js
+++ StoreWebService/models/Mobile.js
@@ -10,7 +10,7 @@ removeMobile:function(id,callback){
 },
  addMobile:function(iduser,Mobile, callback){
         console.log(iduser);
-        return db.query("insert into mobile (mobile, status, iduser) values(?,?,?)",[Mobile.mobile,Mobile.status,iduser],callback);
+        return db.query("insert into mobile (mobile,iduser) values(?,?,?)",[Mobile.mobile,iduser],callback);
     }
 };
  module.exports=Mobile;
